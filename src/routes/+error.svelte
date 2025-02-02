88820598810594db2d8b71c4b625fb08d703094d
권세건 <39672943+honeybugs@users.noreply.github.com>
2025-02-02 14:41:22 +0900

    init

---
+++ src/routes/+error.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { page } from '$app/state';
+
+</script>
+
+<div class="flex h-full w-full flex-col items-center justify-center">
+  {#if page.status === 404}
+    <h1 class="text-3xl font-bold text-zinc-900">페이지를 찾을 수 없습니다</h1>
+    <p class="text-zinc-700 mt-4">요청하신 페이지가 존재하지 않습니다.</p>
+  {:else}
+    <h1 class="text-3xl font-bold text-zinc-900">서버 오류가 발생했습니다</h1>
+    <p class="text-zinc-700 mt-4">죄송합니다. 서버에 문제가 발생했습니다. 잠시 후 다시 시도해주세요.</p>
+    {#if page.error?.message}
+      <p class="error-message">{page.error.message}</p>
+    {/if}
+  {/if}
+  <a href="/" class="text-blue-700 mt-8">메인 페이지로 돌아가기</a>
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,57 @@
+<script>
+	import '../app.css';
+	import { page } from '$app/state';
+
+	let { children } = $props();
+
+	let pathname = $state(page.url.pathname);
+
+	let users = [12312321, 12523432, 25235235, 23523523, 325235, 32523523523532, 523523523, 523523];
+</script>
+
+<header class="fixed left-0 top-0 flex h-10 w-full border-b border-zinc-200 bg-zinc-100 z-10" >
+	<p
+		class="flex w-[200px] items-center justify-center border-r border-zinc-200 text-center text-lg font-bold text-zinc-800"
+	>
+		Bento
+	</p>
+	<div class="flex items-center gap-4 pl-4 text-sm">
+		<a
+			href="/cohort"
+			class="w-fit text-center {pathname === '/cohort'
+				? 'font-semibold text-zinc-800'
+				: 'text-zinc-700'}">코호트 정의</a
+		>
+		<a
+			href="/inference"
+			class="w-fit text-center {pathname === '/inference'
+				? 'font-semibold text-zinc-800'
+				: 'text-zinc-700'}">모델 인퍼런스</a
+		>
+	</div>
+</header>
+<div class="fixed left-0 top-10 flex h-full w-[200px] flex-col border-r border-zinc-200">
+	<div class=" flex w-full px-2 py-2">
+		<input
+			type="text"
+			class="h-8 w-full rounded-sm border border-zinc-200 bg-zinc-50 text-left shadow-sm placeholder:text-xs text-xs"
+			placeholder="유저 ID를 입력하세요."
+		/>
+
+		<!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+				<path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+			</svg> -->
+	</div>
+	<div class="h-full w-full px-2">
+		<div class="flex flex-col rounded-sm border-r border-t border-l border-zinc-200 bg-zinc-50">
+			{#each users as user}
+				<button class="w-full border-b border-zinc-200 px-2 py-2 text-left text-xs overflow-wrap">
+					남 (만 22) | {user} 
+				</button>
+			{/each}
+		</div>
+	</div>
+</div>
+<div class="absolute left-[200px] top-10 h-[calc(100vh-30px)] w-[calc(100vw-200px)]">
+	{@render children()}
+</div>
