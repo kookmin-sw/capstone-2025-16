8ef293d9bc798fca356f66d271a970caa6e4f297
HYEJIN <imagine@kookmin.ac.kr>
2025-02-04 14:26:20 +0900

    Feat: 코호트 목록 UI 구현
    - 코호트 목록은 `ID`, `Name`, `Description`, `Author`, `CreatedAt`, `UpdatedAt` 필드로 구성
    
    - 검색버튼 클릭 시 `Name`, `Description` 필드에서 검색어 필터링하여 반환

---
+++ src/routes/cohort/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let data;
+</script>
+
+<div class="layout">
+  <slot />
+</div>
+
+<style>
+  .layout {
+    padding-top: 30px; /* 상단 패딩 */
+    max-width: 1200px;
+    margin: 0 auto; /* 가운데 정렬 */
+    font-family: Arial, sans-serif; /* 기본 폰트 */
+  }
+</style>
