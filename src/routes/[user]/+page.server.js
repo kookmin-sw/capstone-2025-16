91136d144aa13f4243ba4d80e348d5df9e94218e
rktlskan021 <rktlskan021@gmail.com>
2025-02-11 16:00:31 +0900

    feat: user-testdata api 생성
    user-testdata 안에 있는 데이터를 api 형식으로 불러오기 기능 구현

--- src/routes/[user]/+page.server.js
+++ src/routes/[user]/+page.server.js
@@ -1,6 +1,8 @@
 export async function load({fetch, params}) {
-    const res = await fetch("/api/userdata"); // ✅ SSR에서도 문제없이 실행
+    const res = await fetch("/api/userdata");
     const userData = await res.json();
     const user = userData.find((user) => user.id == params.user);
-    return { user };
+    const userId = user.userid;
+    const userCdms = userData.filter((user) => user.userid == userId);
+    return { user, userCdms };
 }
--- src/routes/api/userdata/+server.js
+++ src/routes/api/userdata/+server.js
@@ -1,5 +1,5 @@
 export async function GET(event) {
-    const res = await event.fetch('http://localhost:5173/user-testdata.json'); // ✅ 절대 경로 사용
+    const res = await event.fetch('http://localhost:5173/user-testdata.json');
     if (!res.ok) {
         return new Response(JSON.stringify({ error: "Failed to load data" }), {
             status: 500,
@@ -7,7 +7,7 @@ export async function GET(event) {
         });
     }
 
-    const data = await res.json(); // ✅ 데이터를 JSON으로 변환
+    const data = await res.json();
 
     return new Response(JSON.stringify(data), {
         headers: { "Content-Type": "application/json" }
