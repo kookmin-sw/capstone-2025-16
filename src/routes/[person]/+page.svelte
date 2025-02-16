0ea36faabf9ad405e7c728953cd33aa9c6afc2e8
rktlskan021 <rktlskan021@gmail.com>
2025-02-16 14:46:50 +0900

    feat: visit testdata api 추가
    visit-testdata 테스트 데이터 추가
    기존 CDM 캘린더 UI 삭제
    visitdata GET API 구현

---
+++ src/routes/[person]/+page.svelte
@@ -0,0 +1,51 @@
+<script>
+    import { onMount } from "svelte";
+	import {tooltip} from '$lib/components/tootip.js';
+
+    export let data;
+    const tables = ['Person', 'Condition', 'Drug'];
+
+    let clickIndex = 0;
+    function setIndex(index) {
+        clickIndex = index;
+    }
+</script>
+
+<div class="flex h-full w-full flex-col items-center border border-black">
+    <div class="flex h-[30vh] w-full flex-col items-center justify-center border border-black">
+        <div id="top-box" class="relative w-[90%] h-[70%] flex border border-black overflow-x-scroll justify-center">
+            
+        </div>
+    </div>
+
+    <div class="flex h-[70vh] w-full items-center justify-center border border-black flex-col">
+        <!-- <div class="w-[90%]">{data.user.name} / {data.user.date} / {data.user.gender} / {data.user.age}</div> -->
+        <div class="h-[90%] w-[90%] border border-black">
+            <ul class="flex border-b border-black">
+                {#each tables as table, index}
+                    <li>
+                        <button
+                            class="h-[30px] w-[100px] border border-black text-center"
+                            class:bg-zinc-300={clickIndex === index}
+                            class:bg-white={clickIndex !== index}
+                            class:font-semibold={clickIndex === index}
+                            class:font-normal={clickIndex !== index}
+                            on:click={() => setIndex(index)}>{table}</button
+                        >
+                    </li>
+                {/each}
+            </ul>
+        </div>
+    </div>
+</div>
+
+<style>
+    #top-box::-webkit-scrollbar {
+        display: none; /* 크롬, 사파리 */
+    }
+
+    #top-box {
+        -ms-overflow-style: none; /* IE, Edge */
+        scrollbar-width: none; /* 파이어폭스 */
+    }
+</style>
---
+++ src/routes/api/visitdata/+server.js
@@ -0,0 +1,15 @@
+export async function GET(event) {
+    const res = await event.fetch('http://localhost:5173/visit-testdata.json');
+    if (!res.ok) {
+        return new Response(JSON.stringify({ error: "Failed to load data" }), {
+            status: 500,
+            headers: { "Content-Type": "application/json" }
+        });
+    }
+
+    const data = await res.json();
+
+    return new Response(JSON.stringify(data), {
+        headers: { "Content-Type": "application/json" }
+    });
+}
