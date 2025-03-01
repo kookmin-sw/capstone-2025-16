0cd4eb685d4011c34190cdc3663c7dd71b436a73
HYEJIN <imagine@kookmin.ac.kr>
2025-03-01 10:02:05 +0900

    Feat: `DonutChartGroup` 추가
    여러 개의 도넛 차트를 하나의 카드에 띄울 경우의 기능을 분리
    - 여러 개의 도넛 차트 띄우기
    - label의 합집합으로 범례 표시
    - 각 도넛 차트 아래에 코호트 이름을 표시해서 각각을 구분

---
+++ src/lib/components/DonutChartGroup.svelte
@@ -0,0 +1,55 @@
+<script>
+  import DonutChart from './DonutChart.svelte';
+  
+  export let chartsData = []; // [{data: {}, cohortName: ''}, ...]
+  export let showLegend = true;
+  
+  let hoveredLabel = null;
+  
+  // 모든 차트에서 사용할 공통 색상 매핑
+  const colorMap = {
+    'Male': '#4299E1',     // blue-500
+    'Female': '#F9A7B0',   // pink
+    'Unknown': '#808080',  // gray
+    'Alive': '#48BB78',    // green-500
+    'Deceased': '#F56565'  // red-500
+  };
+</script>
+
+<div class="flex flex-col md:flex-row items-center w-full gap-4">
+  <div class="flex flex-wrap justify-center items-center flex-1 gap-8">
+    {#each chartsData as {data, cohortName}, i}
+      <div class="flex flex-col items-center gap-2">
+        <DonutChart 
+          {data}
+          {hoveredLabel}
+          {colorMap}
+          on:labelHover={(e) => hoveredLabel = e.detail}
+          showLegend={false}
+          size={200}
+        />
+        <span class="text-sm font-medium text-gray-600">{cohortName}</span>
+      </div>
+    {/each}
+  </div>
+
+  {#if showLegend && chartsData.length > 0}
+    <div class="flex flex-row md:flex-col gap-3 mt-4 md:mt-0">
+      {#each Object.entries(chartsData[0].data) as [label, _]}
+        <div 
+          class="flex items-center gap-2 text-sm text-gray-600 cursor-pointer"
+          role="button"
+          tabindex="0"
+          on:mouseenter={() => hoveredLabel = label}
+          on:mouseleave={() => hoveredLabel = null}
+        >
+          <div 
+            class="w-3 h-3 rounded-full" 
+            style:background-color={colorMap[label]}
+          ></div>
+          <span class="font-medium">{label}</span>
+        </div>
+      {/each}
+    </div>
+  {/if}
+</div> 
