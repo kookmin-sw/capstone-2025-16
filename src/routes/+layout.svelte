e3126a97a6f6067a6431ee0e93252adcc572b135
HYEJIN <imagine@kookmin.ac.kr>
2025-03-05 20:51:47 +0900

    Chore: 레이아웃 임시 수정

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,41 +12,17 @@
 
 
 <style>
-	.button {
-		display: inline-block;
-		padding: 7px 20px;
-		margin: 10px;
-		font-size: 13px;
-		font-weight: bold;
-		color: black;
-		background-color: white;
-		border: 2px solid grey;
-		border-color: black;
-		border-radius: 8px;
-		cursor: pointer;
-		text-align: center;
-		transition: background-color 0.3s ease;
-	}
-	.button-container {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
-
-	.button:hover {
-		background-color: lightgrey;
-	}
-
 	:global(body) {
 		overflow: hidden; /* 전체 페이지의 스크롤 방지 */
 	}
 
 	.content-area {
 		position: absolute;
-		left: 200px;
+		left: 0px;
 		top: 40px;
 		height: calc(100vh - 40px);
-		width: calc(100vw - 200px);
+		width: 100vw;
+		/* width: calc(100vw - 200px); */
 		overflow-x: auto; /* 가로 스크롤 허용 */
 		overflow-y: auto; /* 세로 스크롤 허용 */
 	}
@@ -73,7 +49,8 @@
 		>
 	</div>
 </header>
-<div class="fixed left-0 top-10 flex h-full w-[200px] flex-col border-r border-zinc-200">
+
+<!-- <div class="fixed left-0 top-10 flex h-full w-[200px] flex-col border-r border-zinc-200">
 	<div>
 		<div class="button-container">
 		<button class="button" onclick={() => goto('/chart')}>
@@ -88,9 +65,9 @@
 			placeholder="유저 ID를 입력하세요."
 		/>
 
-		<!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+		<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
 				<path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-			</svg> -->
+			</svg>
 	</div>
 	<div class="h-full w-full px-2">
 		<div class="flex flex-col rounded-sm border-r border-t border-l border-zinc-200 bg-zinc-50 max-h-full overflow-y-auto">
@@ -103,7 +80,7 @@
 			{/each}
 		</div>
 	</div>
-</div>
+</div> -->
 <div class="content-area">
 	{@render children()}
 </div>
