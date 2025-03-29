fe9c57468006cd741efe41157e3af46ab344d175
rktlskan021 <rktlskan021@gmail.com>
2025-03-29 22:07:06 +0900

    Feat: 코호트 Person ID 검색 기능 추가

--- frontend/src/routes/cohort/[cohortID]/+layout.svelte
+++ frontend/src/routes/cohort/[cohortID]/+layout.svelte
@@ -1,10 +1,29 @@
 <script>
     import { page } from '$app/stores';
 	import { goto } from '$app/navigation';
+	import { filter } from 'd3';
+	import { onMount } from 'svelte';
 
+	let { children, data } = $props();
     const cohortID = $page.params.cohortID;
+	let searchQuery = $state("");
+	let filteredData = $state([]);
+	
+	// 검색어에 따라 데이터를 필터링
+	function filterData() {
+		if(searchQuery.length === 0){
+			filteredData = data.userData;
+			return;	
+		}
 
-    let { children, data } = $props();
+		filteredData = data.userData.filter((item) =>
+			item.personid === parseInt(searchQuery)
+		);
+	}
+
+	onMount(() => {
+		filteredData = data.userData;
+	});
 </script>
 
 <div class="fixed left-0 top-10 flex h-full w-[200px] flex-col border-r border-zinc-200">
@@ -22,19 +41,24 @@
 		<input
 			type="text"
 			class="h-8 w-full rounded-sm border border-zinc-200 bg-zinc-50 text-left shadow-sm placeholder:text-xs text-xs"
-			placeholder="유저 ID를 입력하세요."
+			placeholder="Enter Person ID"
+			bind:value={searchQuery}
+			onkeydown={(e) => {
+				if(e.key === "Enter") filterData();
+			}}
 		/>
-
-		<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+		<button onclick={filterData} aria-label="Search-Person-ID">
+			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
 				<path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
 			</svg>
+		</button>
 	</div>
 	<div class="h-full w-full px-2">
 		<div class="flex flex-col rounded-sm border-r border-t border-l border-zinc-200 bg-zinc-50 max-h-full overflow-y-auto">
-			{#each data.userData as user}
+			{#each filteredData as user}
 				<a href="/cohort/{cohortID}/{user.personid}">
 					<button class="w-full border-b border-zinc-200 px-2 py-2 text-left text-xs overflow-wrap">
-						{user.gender} (만 {user.age}) | {user.id}
+						{user.gender} (만 {user.age}) | {user.personid}
 					</button>
 				</a>
 			{/each}
