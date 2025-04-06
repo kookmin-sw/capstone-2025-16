cfa27f27f809bf72e7b1da9819e5a57eebb9690c
hyejin0904 <imagine@kookmin.ac.kr>
2025-04-07 00:40:56 +0900

    Feat: 코호트 기본 페이지 `Default Chart` 탭 구현
    기존 analysis page에 있던 Default 통계 내용을 이전하였습니다.

--- frontend/src/routes/cohort/[cohortID]/+page.svelte
+++ frontend/src/routes/cohort/[cohortID]/+page.svelte
@@ -1,6 +1,18 @@
 <script>
     import analysisData from '$lib/data/singleCohortAnalysisTest.json';
+    import ChartCard from "$lib/components/ChartCard.svelte";
+    import DataTable from "$lib/components/DataTable.svelte";
+    import { AGE_GROUPS, SINGLE_DATA_COLOR } from '$lib/constants.js';
+    import DonutChart from '$lib/components/Charts/DonutChart/DonutChart.svelte';
+    import SingleDonutChartWrapper from '$lib/components/Charts/DonutChart/SingleDonutChartWrapper.svelte';
+    import { transformDonutChartToTableData } from '$lib/components/Charts/DonutChart/donutChartTransformer.js';
+    import LineChart from '$lib/components/Charts/LineChart/LineChart.svelte';
+    import { transformLineChartToTableData } from "$lib/components/Charts/LineChart/lineChartTransformer.js";
+    import BarChart from "$lib/components/Charts/BarChart/BarChart.svelte"
+    import BarChartWrapper from "$lib/components/Charts/BarChart/BarChartWrapper.svelte"
+    import BarChartTableView from '$lib/components/Charts/BarChart/BarChartTableView.svelte';
     import Footer from '$lib/components/Footer.svelte';
+    
     let activeTab = 'default';
     const tabs = [
 		{ key: 'definition', label: 'Definition' },
@@ -8,6 +20,18 @@
 		{ key: 'default', label: 'Default Chart' },
 		{ key: 'customizable', label: 'Customizable Chart' },
 	];
+    
+    let isTableView = {
+        genderRatio: false,
+        mortality: false,
+        visitTypeRatio: false,
+        firstOccurrenceAge: false,
+        visitCount: false,
+        topTenDrugs: false,
+        topTenConditions: false,
+        topTenProcedures: false,
+        topTenMeasurements: false
+    };
     function switchTab(tab) { // 탭을 바꿀 때 활성 탭 상태 변경 함수
         activeTab = tab;
     }
@@ -66,6 +90,241 @@
     {#if activeTab === 'definition'}
         <div class="p-6">
             코호트 구성 탭 화면
+        </div>
+    {/if}
+
+    {#if activeTab == 'default'}
+        <div class="w-full">
+            <div class="grid grid-cols-6 gap-4">
+                <ChartCard
+                    title="Gender Ratio"
+                    description="The ratio of genders within the cohort."
+                    type="third"
+                    hasTableView={true}
+                    isTableView={isTableView.gender}
+                    hasXButton={false}
+                    on:toggleView={({ detail }) => isTableView.gender = detail}
+                >
+                    <SingleDonutChartWrapper data={analysisData.statistics.gender} />
+
+                    <div slot="table" class="w-full h-full flex flex-col pt-2">
+                        <DataTable data={transformDonutChartToTableData({
+                            cohortName: analysisData.basicInfo.name,
+                            data: analysisData.statistics.gender,
+                            totalPatients: analysisData.totalPatients
+                        })} 
+                        />
+                    </div>
+                </ChartCard>
+
+                <ChartCard
+                    title="Mortality"
+                    description="The percentage of patients within the cohort who have died."
+                    type="third"
+                    hasTableView={true}
+                    isTableView={isTableView.mortality}
+                    hasXButton={false}
+                    on:toggleView={({ detail }) => isTableView.mortality = detail}
+                >
+                    <SingleDonutChartWrapper data={analysisData.statistics.mortality} />
+
+                    <div slot="table" class="w-full h-full flex flex-col pt-2">
+                        <DataTable
+                        data={transformDonutChartToTableData({
+                            cohortName: analysisData.basicInfo.name,
+                            data: analysisData.statistics.mortality,
+                            totalPatients: analysisData.totalPatients
+                        })}
+                        />
+                    </div>
+                </ChartCard>
+
+                <ChartCard
+                    title="Visit Type Ratio"
+                    description="The proportion of different types of medical visits (outpatient, inpatient, emergency room, etc.) that occurred during the cohort period."
+                    type="third"
+                    hasTableView={true}
+                    isTableView={isTableView.visitTypeRatio}
+                    hasXButton={false}
+                    on:toggleView={({ detail }) => isTableView.visitTypeRatio = detail}
+                >
+                    <SingleDonutChartWrapper data={analysisData.statistics.visitType} />
+
+                    <div slot="table" class="w-full h-full flex flex-col pt-2">
+                        <DataTable
+                        data={transformDonutChartToTableData({
+                            cohortName: analysisData.basicInfo.name,
+                            data: analysisData.statistics.visitType,
+                            totalPatients: analysisData.totalPatients
+                        })}
+                        />
+                    </div>
+                </ChartCard>
+
+                <ChartCard
+                    title="Distribution of First Occurrence Age"
+                    description="The age distribution of patients at the time of their first medical visit during the cohort period."
+                    type="half"
+                    hasTableView={true}
+                    isTableView={isTableView.age}
+                    hasXButton={false}
+                    on:toggleView={({ detail }) => isTableView.age = detail}
+                >
+                    <LineChart
+                        data={AGE_GROUPS.map(label => ({
+                            label,
+                            value: analysisData.statistics.age[label] ?? 0,
+                            series: analysisData.basicInfo.name
+                        }))}
+                        cohortColorMap={{ [analysisData.basicInfo.name]: SINGLE_DATA_COLOR }}
+                        showLegend={false}
+                    />
+
+                    <div slot="table" class="w-full h-full flex flex-col p-4">
+                        <DataTable
+                        data={transformLineChartToTableData(
+                            AGE_GROUPS.map(label => ({
+                            label,
+                            value: analysisData.statistics.age[label] ?? 0,
+                            series: analysisData.basicInfo.name
+                            }))
+                        )}
+                        />
+                    </div>
+                </ChartCard>
+
+                <ChartCard
+                    title="Distribution of Visit Count"
+                    description="The distribution of the total number of medical visits made by patients during the cohort period."
+                    type="half"
+                    hasTableView={true}
+                    isTableView={isTableView.visitCount}
+                    hasXButton={false}
+                    on:toggleView={({ detail }) => isTableView.visitCount = detail}
+                >
+                    <LineChart
+                        data={Object.entries(analysisData.statistics.visitCount).map(([count, value]) => ({
+                        label: count,
+                        value: value,
+                        series: analysisData.basicInfo.name
+                        }))}
+                        cohortColorMap={{ [analysisData.basicInfo.name]: SINGLE_DATA_COLOR }}
+                        showLegend={false}
+                    />
+
+                    <div slot="table" class="w-full h-full flex flex-col p-4">
+                        <DataTable
+                            data={transformLineChartToTableData(
+                                Object.entries(analysisData.statistics.visitCount).map(([count, value]) => ({
+                                label: count,
+                                value: value,
+                                series: analysisData.basicInfo.name
+                                }))
+                            )}
+                        />
+                    </div>
+                </ChartCard>
+
+                <ChartCard
+                    title="Top 10 Drugs"
+                    description= "The list of the top 10 most frequently prescribed medications for patients in the cohort."
+                    type="half"
+                    hasTableView={true}
+                    isTableView={isTableView.topTenDrugs}
+                    hasXButton={false}
+                    on:toggleView={({ detail }) => isTableView.topTenDrugs = detail}
+                >
+                    <BarChartWrapper
+                    data={Object.entries(analysisData.statistics.topTenDrugs).map(([name, count]) => ({ name, count }))}
+                    cohortName={analysisData.basicInfo.name}
+                    cohortTotalCount={analysisData.totalPatients}
+                    />
+
+                    <div slot="table" class="w-full h-full flex flex-col p-4 overflow-auto">
+                        <BarChartTableView
+                            data={Object.entries(analysisData.statistics.topTenDrugs).map(([name, count]) => ({ name, count }))}
+                            domainKey="drug"
+                            cohortName={analysisData.basicInfo.name}
+                            cohortTotalCount={analysisData.totalPatients}
+                        />
+                    </div>
+                </ChartCard>
+
+                <ChartCard
+                    title="Top 10 Conditions"
+                    description="The list of the top 10 most frequently diagnosed medical conditions among patients in the cohort."
+                    type="half"
+                    hasTableView={true}
+                    isTableView={isTableView.topTenConditions}
+                    hasXButton={false}
+                    on:toggleView={({ detail }) => isTableView.topTenConditions = detail}
+                >
+                    <BarChartWrapper
+                    data={Object.entries(analysisData.statistics.topTenConditions).map(([name, count]) => ({ name, count }))}
+                    cohortName={analysisData.basicInfo.name}
+                    cohortTotalCount={analysisData.totalPatients}
+                    />
+                
+                    <div slot="table" class="w-full h-full flex flex-col p-4 overflow-auto">
+                        <BarChartTableView
+                            data={Object.entries(analysisData.statistics.topTenConditions).map(([name, count]) => ({ name, count }))}
+                            domainKey="condition"
+                            cohortName={analysisData.basicInfo.name}
+                            cohortTotalCount={analysisData.totalPatients}
+                        />
+                    </div>
+                </ChartCard>
+
+                <ChartCard
+                    title="Top 10 Procedures"
+                    description="The list of the top 10 most frequently performed procedures and medical tests on patients in the cohort."
+                    type="half"
+                    hasTableView={true}
+                    isTableView={isTableView.topTenProcedures}
+                    hasXButton={false}
+                    on:toggleView={({ detail }) => isTableView.topTenProcedures = detail}
+                >
+                    <BarChartWrapper
+                        data={Object.entries(analysisData.statistics.topTenProcedures).map(([name, count]) => ({ name, count }))}
+                        cohortName={analysisData.basicInfo.name}
+                        cohortTotalCount={analysisData.totalPatients}
+                    />
+
+                    <div slot="table" class="w-full h-full flex flex-col p-4 overflow-auto">
+                        <BarChartTableView
+                            data={Object.entries(analysisData.statistics.topTenProcedures).map(([name, count]) => ({ name, count }))}
+                            domainKey="procedure"
+                            cohortName={analysisData.basicInfo.name}
+                            cohortTotalCount={analysisData.totalPatients}
+                        />
+                    </div>
+                </ChartCard>
+
+                <ChartCard
+                    title="Top 10 Measurements"
+                    description="The list of the top 10 most frequently recorded clinical measurements within the cohort."
+                    type="half"
+                    hasTableView={true}
+                    isTableView={isTableView.topTenMeasurements}
+                    hasXButton={false}
+                    on:toggleView={({ detail }) => isTableView.topTenMeasurements = detail}
+                >
+                    <BarChartWrapper
+                        data={Object.entries(analysisData.statistics.topTenMeasurements).map(([name, count]) => ({ name, count }))}
+                        cohortName={analysisData.basicInfo.name}
+                        cohortTotalCount={analysisData.totalPatients}
+                    />
+
+                    <div slot="table" class="w-full h-full flex flex-col p-4 overflow-auto">
+                        <BarChartTableView
+                            data={Object.entries(analysisData.statistics.topTenMeasurements).map(([name, count]) => ({ name, count }))}
+                            domainKey="measurement"
+                            cohortName={analysisData.basicInfo.name}
+                            cohortTotalCount={analysisData.totalPatients}
+                        />
+                    </div>
+                </ChartCard>
+            </div>
         </div>
     {/if}
 
