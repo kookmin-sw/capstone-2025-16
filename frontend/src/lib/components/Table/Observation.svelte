14e02afafeea7e2cf668f31dbdaac9419470d81c
rktlskan021 <rktlskan021@gmail.com>
2025-04-12 18:48:11 +0900

    Refactor: Observatoin 컴포넌트 수정

--- frontend/src/lib/components/Table/Observation.svelte
+++ frontend/src/lib/components/Table/Observation.svelte
@@ -9,18 +9,18 @@
     <table class="observation-table w-full border border-gray-200 text-sm text-left mb-6">
       <thead class="bg-gray-100">
         <tr>
-          <th class="px-4 py-2 border-b">Date</th>
           <th class="px-4 py-2 border-b">Concept ID</th>
+          <th class="px-4 py-2 border-b">Date</th>
           <th class="px-4 py-2 border-b">Value</th>
         </tr>
       </thead>
       <tbody>
         {#each observation as obs}
           <tr class="hover:bg-gray-50">
-            <td class="px-4 py-2 border-b">{obs.observation_date}</td>
             <td class="px-4 py-2 border-b">{obs.observation_concept_id}</td>
+            <td class="px-4 py-2 border-b">{obs.observation_date}</td>
             <td class="px-4 py-2 border-b">
-              <span class="font-medium">{obs.value_as_string}</span>
+              <span class="font-medium">{obs.value_as_string} {(String.toString(obs.unit_source_value) === "N/A") ? obs.unit_source_value : "" }</span>
             </td>
           </tr>
         {/each}
