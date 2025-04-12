4353dfa7850365864e98a02a1d4dac8bc44a0a23
rktlskan021 <rktlskan021@gmail.com>
2025-04-12 18:42:19 +0900

    Refactor: Measurement 컴포넌트 수정

--- frontend/src/lib/components/Table/Measurement.svelte
+++ frontend/src/lib/components/Table/Measurement.svelte
@@ -9,17 +9,17 @@
     <table class="measurement-table w-full border border-gray-200 text-sm text-left">
       <thead class="bg-gray-100">
         <tr>
-          <th class="px-4 py-2 border-b">Date</th>
           <th class="px-4 py-2 border-b">Concept ID</th>
+          <th class="px-4 py-2 border-b">Date</th>
           <th class="px-4 py-2 border-b">Value</th>
         </tr>
       </thead>
       <tbody>
         {#each measurement as measure}
           <tr class="hover:bg-gray-50">
-            <td class="px-4 py-2 border-b">{measure.measurement_date}</td>
             <td class="px-4 py-2 border-b">{measure.measurement_concept_id}</td>
-            <td class="px-4 py-2 border-b">{measure.value_as_number}</td>
+            <td class="px-4 py-2 border-b">{measure.measurement_date}</td>
+            <td class="px-4 py-2 border-b">{measure.value_as_number} {measure.unit_source_value}</td>
           </tr>
         {/each}
       </tbody>
