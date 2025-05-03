acd74cc6842be7b4f946ddee40a50303879031c7
bgw4399 <104902657+bgw4399@users.noreply.github.com>
2025-05-03 14:25:00 +0900

    domain => domain_name

--- backend/api/src/db/clickhouse.sql
+++ backend/api/src/db/clickhouse.sql
@@ -414,13 +414,13 @@ CREATE TABLE `cohort_concept` (
 CREATE TABLE IF NOT EXISTS feature_extraction
 (
     cohort_id   UUID,                      
-    domain      LowCardinality(String),
+    domain_name      LowCardinality(String),
     rank        Int64,                     
     concept_id  Int64,
     influence Int64,
     execution_time Int64
 )
-ORDER BY (cohort_id, domain, rank);
+ORDER BY (cohort_id, domain_name, rank);
 
 CREATE OR REPLACE FUNCTION _to_date AS (a) -> toDate32(a);
 
