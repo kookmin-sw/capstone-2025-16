63dcdf19242c7624118f1fd239f3a16a7cb8d2ff
rktlskan021 <rktlskan021@gmail.com>
2025-05-06 01:05:06 +0900

    Refacgor: Bio Signal font size 조절

--- frontend/src/lib/components/Table/BioSignal.svelte
+++ frontend/src/lib/components/Table/BioSignal.svelte
@@ -1,34 +1,26 @@
 <script>
 	import { xmlParseToJson } from '$lib/ecgparse.js';
-  import ECGViewer from '$lib/components/ECGViewer.svelte';
+  	import ECGViewer from '$lib/components/ECGViewer.svelte';
 
 	export let bioSignal;
 </script>
 
-<div class="bio-signal-container">
+<div class="bioContainer flex flex-col bg-white border border-gray-300 rounded-lg shadow-md p-6 relative mb-1 w-full">
 	<h2 class="title">Bio Signal Information</h2>
 
 	{#each bioSignal as signal, i}
 		<div class="bio-signal-row">
-			<span class="info"><strong>Date:</strong> {signal.bio_signal_date}</span>
+			<span class="text-sm"><strong>Date:</strong> {signal.bio_signal_date}</span>
 			<span class="divider">|</span>
-			<span class="info"><strong>Concept ID:</strong> {signal.bio_signal_concept_id}</span>
+			<span class="text-sm"><strong>Concept ID:</strong> {signal.bio_signal_concept_id}</span>
 			<span class="divider">|</span>
-			<span class="info"><strong>Source Value:</strong> {signal.bio_signal_source_value}</span>
+			<span class="text-sm"><strong>Source Value:</strong> {signal.bio_signal_source_value}</span>
 		</div>
 		<ECGViewer filePath="/140474_20160807_0830.xml"/>
 	{/each}
 </div>
 
 <style>
-	.bio-signal-container {
-		padding: 16px;
-		background: #f9f9f9;
-		border-radius: 8px;
-		border: 1px solid #ddd;
-		max-width: 100%;
-	}
-
 	.title {
 		font-size: 1.3rem;
 		font-weight: bold;
@@ -44,12 +36,6 @@
 		background: #fff;
 		border-bottom: 1px solid #ddd;
 		white-space: nowrap;
-	}
-
-	.info {
-		font-weight: normal;
-		font-size: 1.1rem;
-		margin-right: 10px;
 	}
 
 	.divider {
