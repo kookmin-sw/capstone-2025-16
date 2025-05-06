b8b81eb44a37f8da0929ab3484935dc16ce2d30b
rktlskan021 <rktlskan021@gmail.com>
2025-05-06 14:00:32 +0900

    Feat: 개인 통계 API 구현:

--- frontend/src/routes/cohort/[cohortID]/[person]/+page.server.js
+++ frontend/src/routes/cohort/[cohortID]/[person]/+page.server.js
@@ -4,6 +4,11 @@ export async function load({fetch, params}) {
     
     const res2 = await fetch(`/api/visitdata/${params.person}`);
     const visitData = await res2.json();
-    
-    return { personInfo, visitData };
+
+    const res3 = await fetch(`/api/personstatistics/${params.person}`)
+    const personStatistics = await res3.json();
+
+    console.log(personInfo);
+
+    return { personInfo, visitData, personStatistics };
 }
