c0d9c79d78ca61e64142b0a04fcde82f6d4efcbd
rktlskan021 <rktlskan021@gmail.com>
2025-05-06 16:23:28 +0900

    Feat: cohort 통계 API 적용

--- frontend/src/routes/cohort/[cohortID]/+page.server.js
+++ frontend/src/routes/cohort/[cohortID]/+page.server.js
@@ -1,5 +1,8 @@
 export async function load({ fetch, params }) {
     const res = await fetch(`/api/cohortinfo/${params.cohortID}`); // ✅ SSR에서도 문제없이 실행
     const cohortinfo = await res.json();
-    return { cohortinfo };
+
+    const res2 = await fetch(`/api/cohortstatistics/${params.cohortID}`); // ✅ SSR에서도 문제없이 실행
+    const cohortStatistics = await res2.json();
+    return { cohortinfo, cohortStatistics };
 }
