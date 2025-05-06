f1250a29a58000067ec313b08a43add8490753b4
cg10036 <cg10036@gmail.com>
2025-05-06 18:58:35 +0900

    Chore: 예전 코드 제거

--- backend/cohort-query-service-nodejs/src/circe/resources/cohortdefinition/sql/dateOffsetStrategy.sql
+++
@@ -1,6 +0,0 @@
--- date offset strategy
-
-select event_id, person_id, 
-  case when DATEADD(day,@offset,@dateField) > op_end_date then op_end_date else DATEADD(day,@offset,@dateField) end as end_date
-INTO #strategy_ends
-from @eventTable;
