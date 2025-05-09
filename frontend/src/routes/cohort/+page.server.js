b17797b85d849fe108585d1334e7af59e10690ac
rktlskan021 <rktlskan021@gmail.com>
2025-05-10 02:30:35 +0900

    Feat: 코호트 리스트 API 로딩창 적용

--- frontend/src/routes/cohort/+page.server.js
+++
@@ -1,5 +0,0 @@
-export async function load({ fetch }) {
-    const res = await fetch('/api/cohortlistdata'); // ✅ SSR에서도 문제없이 실행
-    const cohortList = await res.json();
-    return { cohortList };
-}
