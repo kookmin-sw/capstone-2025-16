54a8385c7d1e92b3f7a1097095186dc5766b82df
rktlskan021 <rktlskan021@gmail.com>
2025-05-12 15:34:26 +0900

    Refactor: 코드 리펙터링

--- frontend/src/routes/custom-chart/+page.svelte
+++ frontend/src/routes/custom-chart/+page.svelte
@@ -134,7 +134,7 @@
                 <td class="py-3 px-4 text-sm text-gray-500">{item.statistics_id}</td>
                 <td class="py-3 px-4">
                   <a 
-                    href={`/custom-chart/${item.id}`} 
+                    href={`/custom-chart/${item.statistics_id}`} 
                     class="text-sm font-medium text-blue-600 hover:text-blue-800 hover:underline"
                   >
                     {item.name}
