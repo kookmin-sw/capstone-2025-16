0788e33a891ca694a410e5c104d31e24c48f69af
hyejin0904 <imagine@kookmin.ac.kr>
2025-05-15 03:11:41 +0900

    Feat: 커스텀 차트 페이지 내 삭제 기능 추가

--- frontend/src/routes/custom-chart/[statisticsId]/+page.svelte
+++ frontend/src/routes/custom-chart/[statisticsId]/+page.svelte
@@ -192,6 +192,47 @@
 						</div>
 						<div class="font-medium text-blue-600">{cumtomInfo.name}</div>
 					</div>
+					<div class="flex items-center gap-4">
+						<!-- 커스텀 차트 페이지 삭제 버튼 -->
+						<button
+							onclick={async () => {
+								const confirmDelete = confirm("Are you sure you want to delete this custom chart set? This action cannot be undone.");
+								if (!confirmDelete) return;
+
+								await fetch(`https://bento.kookm.in/api/statistics/${cumtomInfo.statistics_id}`, {
+									method: 'DELETE'
+								})
+									.then(() => {
+										goto('/custom-chart');
+									})
+									.catch((error) => {
+										console.error('Error deleting custom target set:', error);
+									});
+							}}
+							class="group relative flex items-center justify-center transition-colors hover:bg-red-50 hover:text-red-500"
+							title= "Delete Target Set"
+						>
+							<svg
+								class="h-4 w-4"
+								viewBox="0 0 24 24"
+								fill="none"
+								stroke="currentColor"
+								stroke-width="2"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+							>
+								<polyline points="3 6 5 6 21 6"></polyline>
+								<path
+								d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"
+								></path>
+							</svg>
+							<span
+								class="absolute left-1/2 top-full mt-2 -translate-x-1/2 transform rounded bg-white p-1 text-xs text-gray-700 opacity-0 shadow-md transition-opacity group-hover:opacity-100"
+							>
+								Delete
+							</span>
+						</button>
+					</div>
 				</div>
 
 				<div class="overflow-y-auto p-4">
